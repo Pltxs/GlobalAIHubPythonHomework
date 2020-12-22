90f52176a73a018e2ef8cfdce323cb7e1ce49750
Oktay Polat <59055683+Pltxs@users.noreply.github.com>
2020-12-22 11:09:14 +0300

    Add files via upload

---
+++ fiveInputs.py
@@ -0,0 +1,16 @@
+#Beginner approach:
+value1= input("Write what you want: ")
+value2= input("Write what you want: ")
+value3= input("Write what you want: ")
+value4= input("Write what you want: ")
+value5= input("Write what you want: ")
+print("value1: {0}\nvalue2: {1}\nvalue3: {2}\nvalue4: {3}\nvalue5: {4}"
+    .format(value1,value2,value3,value4,value5))
+
+#Another approach:
+# values=[]
+# for i in range(0,5):
+#     value=input("Write what you want: ")
+#     values.append(value)
+# print("value1: {0}\nvalue2: {1}\nvalue3: {2}\nvalue4: {3}\nvalue5: {4}"
+#     .format(values[0],values[1],values[2],values[3],values[4]))
