df3bc844dcd6436e3001babe0ae8ffcda170937a
Oktay Polat <59055683+Pltxs@users.noreply.github.com>
2020-12-27 20:22:44 +0300

    Delete homework2.py

--- homework2.py
+++
@@ -1,8 +0,0 @@
-person=[]
-firstName=person.append("Firstname: " + input("Write your name: "))
-lastName=person.append("Lastname: " + input("Write your lastname: "))
-age=person.append("Age: " + input("Write your age: "))
-birthDate=person.append("Birthyear: " + input("Write your birth year: "))
-
-for i in person:
-    print(i)
